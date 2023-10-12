80d5819f29d5af2453bf170c58badcb69c4f9c92
Soledad Pizarro <soledad.pizarro21@gmail.com>
2023-10-11 22:30:49 -0300

    Correcciones entrega

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let form = document.querySelector('form');
 let cantidadInput = document.getElementById('cantidad');
 let monedaOrigenSelect = document.getElementById('moneda-origen');
@@ -7,10 +6,10 @@ let resultadoDiv = document.getElementById('resultado');
 
 const tasasDeCambio = {
     usd: 1.00,
-    eur: 1.10,
-    real: 0.70,
-    chilenos: 720,
-    libras: 1.25
+    eur: 1.15,
+    real: 0.19,
+    chilenos: 0.0013,
+    libras: 1.36,
 };
 
 form.addEventListener('submit', function (e) {
@@ -25,51 +24,63 @@ form.addEventListener('submit', function (e) {
     resultadoDiv.textContent = 'Tu cambio es: ' + cambio.toFixed(1) + ' ' + monedaDestino;
 });
 
+function calcularCambio(cantidad, monedaOrigen, monedaDestino) {
+    if (!(monedaOrigen in tasasDeCambio) || !(monedaDestino in tasasDeCambio)) {
+        return "Moneda no válida";
+    }
+    const tasaOrigen = tasasDeCambio[monedaOrigen];
+    const tasaDestino = tasasDeCambio[monedaDestino];
+    const valorCambio = cantidad * (tasaDestino / tasaOrigen);
+    return valorCambio.toFixed(1);
+}
+
+
 const botonCarrito = document.getElementById("add-carrito");
-botonCarrito.onclick = function() {
+botonCarrito.onclick = function () {
     agregarAlCarrito();
 };
-function agregarAlCarrito() {
-    const cantidad = document.getElementById("cantidad").value;
+
+const listaCompras = document.getElementById("lista-compras");
+const resultadoFinal = document.getElementById("resultado-final");
+
+const carrito = [];
+
+function actualizarTotal() {
+    let total = 0;
+    carrito.forEach(item => {
+        total += item.total;
+    });
+    resultadoFinal.textContent = "Total: " + total.toFixed(2) + " USD";
+}
+
+document.getElementById("add-carrito").addEventListener("click", () => {
+    const cantidad = parseFloat(document.getElementById("cantidad").value);
     const monedaOrigen = document.getElementById("moneda-origen").value;
     const monedaDestino = document.getElementById("moneda-destino").value;
-    const valorCambio = calcularCambio(cantidad, monedaOrigen, monedaDestino);
-    const carrito = document.getElementById("carrito");
-    const listaCompras = document.getElementById("lista-compras");
-    const total = document.getElementById("total");
-    const nuevoItem = document.createElement("li");
-    nuevoItem.textContent = `${cantidad} ${monedaOrigen} a ${monedaDestino}: ${valorCambio}`;
-    listaCompras.appendChild(nuevoItem);
-    const totalActual = parseFloat(total.textContent);
-    total.textContent = (totalActual + valorCambio).toFixed(1);
-}
 
-    function calcularCambio(cantidad, monedaOrigen, monedaDestino) {
-        if (!(monedaOrigen in tasasDeCambio) || !(monedaDestino in tasasDeCambio)) {
-            return "Moneda no válida";
-        }
-        const tasaOrigen = tasasDeCambio[monedaOrigen];
-        const tasaDestino = tasasDeCambio[monedaDestino];
-        const valorCambio = cantidad * (tasaDestino / tasaOrigen);
-        return valorCambio.toFixed(1); 
+    let total = 0;
+    if (monedaOrigen === "eur" && monedaDestino === "usd") {
+        total = cantidad * 1.15;
+    } else if (monedaOrigen === "real" && monedaDestino === "usd") {
+        total = cantidad * 0.19;
+    } else if (monedaOrigen === "chilenos" && monedaDestino === "usd") {
+        total = cantidad * 0.0013;
+    } else if (monedaOrigen === "libras" && monedaDestino === "usd") {
+        total = cantidad * 1.36;
     }
 
-const operacion = {cantidad, monedaOrigen, monedaDestino, resultado};
-guardarOperacionEnLocalStorage(operacion);
-function guardarOperacionEnLocalStorage(operacion) {
-    const operacionesPrevias = JSON.parse(localStorage.getItem("operaciones")) || [];
-    operacionesPrevias.push(operacion);
-    localStorage.setItem("operaciones", JSON.stringify(operacionesPrevias));
-}
-function mostrarOperacionesDesdeLocalStorage() {
-    const operaciones = JSON.parse(localStorage.getItem("operaciones")) || [];
-    operaciones.forEach(operacion => {
-        const item = document.createElement("li");
-        item.textContent = `${operacion.cantidad} ${operacion.monedaOrigen} a ${operacion.monedaDestino}: ${operacion.resultado.toFixed(1)}`;
-        listaOperaciones.appendChild(item);
-    });
-}
-mostrarOperacionesDesdeLocalStorage();
+    carrito.push({ total });
 
+    const listItem = document.createElement("li");
+    listItem.textContent = `${cantidad} ${monedaOrigen} a USD: ${total.toFixed(1)}`;
+    listaCompras.appendChild(listItem);
 
+    actualizarTotal();
+});
+
+document.getElementById("vaciarCarrito").addEventListener("click", () => {
+    carrito.length = 0;
+    listaCompras.innerHTML = "";
+    resultadoFinal.textContent = "Total:";
+});
 
